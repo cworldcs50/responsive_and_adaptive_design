75590429c8489b1161f4aff36c8cbe621fb9f9d6
cworldcs50 <114599540+cworldcs50@users.noreply.github.com>
2025-12-23 20:19:15 +0200

    make a new repo

---
+++ android/app/src/main/kotlin/com/example/responsive_and_adaptive_design_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.responsive_and_adaptive_design_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ResponsiveAndAdaptiveDesignProject());
+}
+
+class ResponsiveAndAdaptiveDesignProject extends StatelessWidget {
+  const ResponsiveAndAdaptiveDesignProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
